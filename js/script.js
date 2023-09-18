3d12efee784fb6a4b17ff2f2db06499bc8157668
Carla Vidales <carlavidales.ca@gmail.com>
2023-09-18 14:17:22 -0700

    Create showPage function

--- js/script.js
+++ js/script.js
@@ -3,22 +3,40 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
-
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
+// ELEMENT SELECTORS
+// Access ul w/ student-list class
+const studentList = document.querySelector(".page").children[1];
+// Access ul within pagination div
+const paginationList = document.querySelector(".pagination").firstElementChild;
+const studentsPerPage = 9;
 
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+function showPage(array, page) {
+   const start = (page * studentsPerPage) - studentsPerPage;
+   const end = (page * studentsPerPage) - 1;
+   studentList.textContent = "";
 
-
+   for (let i = 0; i < array.length; i++) {
+      if (i >= start && i <= end) {
+         const html = `
+            <li class="student-item cf">
+            <div class="student-details">
+               <img class="avatar" src=${array[i].picture.large} alt="Profile Picture">
+               <h3>${array[i].name.first} ${array[i].name.last}</h3>
+               <span class="email">${array[i].email}</span>
+            </div>
+            <div class="joined-details">
+               <span class="date">Joined ${array[i].registered.date}</span>
+            </div>
+         </li>`;
+         studentList.insertAdjacentHTML("beforeend", html);
+      }
+   }
+}
+showPage(data, 1);
 
 /*
 Create the `addPagination` function
